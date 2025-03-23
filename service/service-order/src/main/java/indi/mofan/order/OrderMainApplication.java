d0fd5482af55d3bfaa6649301ddc557864f38699
mofan <cy.mofan@foxmail.com>
2025-03-23 16:50:31 +0800

    Nacos 服务注册

---
+++ service/service-order/src/main/java/indi/mofan/order/OrderMainApplication.java
@@ -0,0 +1,16 @@
+package indi.mofan.order;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mofan
+ * @date 2025/3/23 16:31
+ */
+@SpringBootApplication
+public class OrderMainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMainApplication.class, args);
+    }
+}
---
+++ service/service-product/src/main/java/indi/mofan/ProductMainApplication.java
@@ -0,0 +1,16 @@
+package indi.mofan;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mofan
+ * @date 2025/3/23 16:35
+ */
+@SpringBootApplication
+public class ProductMainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductMainApplication.class, args);
+    }
+}
